a7ddade99d6954fc971a188e62cedfc5abd242f4
antoniorrm <antonioraimundo007@gmail.com>
2020-07-17 12:52:08 -0300

    add axios api and theme config

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading } from "expo";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/pages/Home";
+
+import {
+	useFonts,
+	Montserrat_400Regular,
+	Montserrat_500Medium,
+	Montserrat_600SemiBold,
+	Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [fontsLoaded] = useFonts({
+		Montserrat_400Regular,
+		Montserrat_500Medium,
+		Montserrat_600SemiBold,
+		Montserrat_700Bold,
+	});
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
+	return (
+		<View style={styles.container}>
+			<Home />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
---
+++ src/theme/index.ts
@@ -0,0 +1,31 @@
+const theme = {
+	fontSize: {
+		big: 96,
+		h1: 28,
+		h2: 24,
+		h3: 16,
+		h4: 14,
+		h5: 12,
+		p: 10,
+	},
+
+	fontFamily: {
+		regular: "Montserrat_400Regular",
+		medium: "Montserrat_500Medium",
+		semiBold: "Montserrat_600SemiBold",
+		bold: "Montserrat_700Bold",
+	},
+
+	colors: {
+		orange500: "#E77E38",
+		red500: "#F5442F",
+		black600: "#141414",
+		black500: "#1D1D1D",
+		black400: "#292929",
+		gray700: "#525252",
+		gray500: "#868686",
+		gray300: "#E9E9E9",
+	},
+};
+
+export { theme };
